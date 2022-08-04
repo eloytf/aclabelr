0e48917dfeb202353b6b10165265811efd4e8eda
eloytf <eloytf@gmail.com>
2022-08-04 19:47:55 +0200

    added hito_cut

--- R/labelutils.R
+++ R/labelutils.R
@@ -19,3 +19,60 @@ split_labels<-function (textfile) {
   lapply(utags,write_lable_tracks)
 
 }
+
+
+#' Title
+#'
+#' @param namehito
+#' @param namecut
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
+cut_label_track<-function(namehito,namecut) {
+
+  hito<-readr::read_delim(file = paste0(namehito,".txt"),delim = "\t",col_names = F)
+  cut<-readr::read_delim(file = paste0(namecut,".txt"),delim = "\t",col_names = F)
+
+  #hito<-intervals2instants(hito)
+
+  hito$X1<-hito$X1-Vectorize(getintervalsize,vectorize.args = "instant")(cut,hito$X1)
+  #no tengo claro que este bien, pero a las malas, hito$x2=hito$x1
+  hito$X2<-hito$X2-Vectorize(getintervalsize,vectorize.args = "instant")(cut,hito$X2)
+
+
+  readr::write_delim(hito,delim = "\t",paste0(namehito,"_recortado",".txt"),col_names = F)
+
+}
+
+
+#' getintervalsize
+#'
+#' @param labeldf
+#' @param instant
+#'
+#' @return value, intervalsize
+#'
+#' @examples
+getintervalsize<-function(labeldf,instant) {
+
+  labeldf<-labeldf[labeldf[,1]<instant,]
+  labeldf$X3<-pmin(instant,labeldf$X2)
+  labeldf$X3<-labeldf$X3-labeldf$X1
+  return(sum(labeldf$X3))
+}
+
+#' intervals2instants
+#'
+#' @param labeldf
+#'
+#' @return a data frame
+#'
+#' @examples
+intervals2instants<-function(labeldf) {
+
+  labeldf[,2]<-labeldf[,1]
+  return(labeldf)
+
+}
